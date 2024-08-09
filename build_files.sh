f555b60a081a4a8fe87d4d89a70c5d745354256b
venvictor602 <venvictor602@gmail.com>
2024-08-09 15:53:51 +0100

    build commit

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,15 @@
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
+
+ #!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Run Django's collectstatic command to gather all static files into STATIC_ROOT
+python manage.py collectstatic --noinput
+
+# Ensure that the output directory exists and move static files there
+mkdir -p staticfiles_build
+cp -r static/* staticfiles_build/
